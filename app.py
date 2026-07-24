e533de2048ce279200b1c55e982b4ec507576003
vscuser <purogram837@gmail.com>
2026-07-24 22:48:08 +0900

    デッドクロス機能追加

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from src.fetch_data import fetch_stock_data
 from src.indicators import add_moving_average
 from src.signals import detect_golden_cross
+from src.signals import detect_dead_cross
 
 df = fetch_stock_data("7203.T")
 
 df = add_moving_average(df)
 
 df = detect_golden_cross(df)
+df = detect_dead_cross(df)
 
-print(df[["Close","MA25","GC"]].tail(20))
+print(df[["Close","MA25","GC","DC"]].tail(30))
--- src/signals.py
+++ src/signals.py
@@ -2,7 +2,13 @@ import pandas as pd
 
 def detect_golden_cross(df:pd.DataFrame) -> pd.DataFrame:
   """ゴールデンクロスを判定する"""
-
   df["GC"] = ((df["MA25"].shift(1)<df["MA75"].shift(1)) & (df["MA25"] >= df["MA75"]))
+
+  return df
+
+def detect_dead_cross(df:pd.DataFrame) -> pd.DataFrame:
+  """デッドクロスを判定する"""
+
+  df["DC"] = ((df["MA25"].shift(1) > df["MA75"].shift(1)) & (df["MA25"] <= df["MA75"]))
 
   return df
